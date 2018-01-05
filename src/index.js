27af04cc382049950b8b88840137fea5f1e1a154
Michel Mattos <mattos.michel@gmail.com>
2018-01-06 01:36:59 +1300

    Added a base style

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { css } from 'react-emotion'
 import { BrowserRouter } from 'react-router-dom'
 import LocationCrafter from './location-crafter'
+import './style.css'
 
 const app = css`
   font-family: Oxygen, sans-serif;
