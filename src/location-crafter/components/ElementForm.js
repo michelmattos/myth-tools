bf63f4c94f0a8c4a218d6227297c41953f259950
Michel Mattos <mattos.michel@gmail.com>
2018-01-06 18:25:29 +1300

    Created a component for creating/editing elements

---
+++ src/location-crafter/components/ElementForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Overlay from './styled/Overlay'
+import BottomPanel from './styled/BottomPanel'
+
+const ElementForm = () =>
+  <Overlay>
+    <BottomPanel>
+      ElementForm
+    </BottomPanel>
+  </Overlay>
+
+export default ElementForm
