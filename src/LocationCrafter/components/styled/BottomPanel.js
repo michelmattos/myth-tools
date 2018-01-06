955e36a15eaca21393731a3e60b41d71e1458a7b
Michel Mattos <mattos.michel@gmail.com>
2018-01-06 19:16:29 +1300

    Changed the styling of Content to allow it to be more reused

--- src/LocationCrafter/components/styled/BottomPanel.js
+++ src/LocationCrafter/components/styled/BottomPanel.js
@@ -3,7 +3,6 @@ import styled from 'react-emotion'
 const BottomPanel = styled('div')`
   background-color: white;
   bottom: 0;
-  padding: 1em;
   position: absolute;
   width: 100%;
 `
--- src/LocationCrafter/components/styled/Content.js
+++ src/LocationCrafter/components/styled/Content.js
@@ -1,7 +1,13 @@
 import styled from 'react-emotion'
 
-const Content = styled('main')`
+const Content = styled('div')`
+  display: flex;
+  flex-direction: column;
   padding: 1em;
+
+  > *:not(:last-child) {
+    margin-bottom: 1em;
+  }
 `
 
 export default Content
