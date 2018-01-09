157eb2893c4ba82b775ea78fa3b060c3675e585e
Michel Mattos <mattos.michel@gmail.com>
2018-01-10 01:41:53 +1300

    Fixed some errors and updated prop type

--- src/common/components/Run.js
+++ src/common/components/Run.js
@@ -2,11 +2,17 @@
 import * as React from 'react'
 
 type Props = {
-  cmd: () => void,
+  cmd: () => any,
 }
 
 class Run extends React.Component<Props> {
   componentDidMount () {
     this.props.cmd()
   }
+
+  render () {
+    return null
+  }
 }
+
+export default Run
