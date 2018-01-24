fd80ae071483e7ff18eea6ee8121b0f44cf32370
Michel Mattos <mattos.michel@gmail.com>
2018-01-24 20:43:22 +1300

    Simplified types for Locaction Crafter module

--- src/locationCrafter/types.js
+++ src/locationCrafter/types.js
@@ -2,13 +2,7 @@
 export type Type = 'CUSTOM' | 'NONE' | 'EXPECTED' | 'SPECIAL' | 'RANDOM' | 'COMPLETE'
 
 export type Element = {
-  id: number,
-  type: Type,
-  name: string,
-  unique: boolean,
-}
-
-export type UnsavedElement = {
+  id?: number,
   type: Type,
   name: string,
   unique: boolean,
