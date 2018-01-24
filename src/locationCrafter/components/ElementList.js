0a6d37ec8472dac88d5a82507c5dfa8364c55957
Michel Mattos <mattos.michel@gmail.com>
2018-01-24 20:56:33 +1300

    Added 'edit element' feature

--- src/locationCrafter/components/ElementList.js
+++ src/locationCrafter/components/ElementList.js
@@ -5,44 +5,63 @@ import List from './styled/List'
 import ListItemButton from './styled/ListItemButton'
 import ElementListItem from './ElementListItem'
 import EditElement from './EditElement'
-import type { Element, UnsavedElement } from '../types'
+import type { Element } from '../types'
 
 type Props = {
   elements: Array<Element>,
-  onElementCreate: (element: UnsavedElement) => any,
+  onSave: (element: Element) => any,
 }
 
 type State = {
   showElementForm: boolean,
+  selectedElement: Element | null,
 }
 
 class ElementList extends React.Component<Props, State> {
   state = {
     showElementForm: false,
+    selectedElement: null
   }
 
   toggleElementForm = () => {
-    this.setState({ showElementForm: !this.state.showElementForm })
+    this.setState({
+      showElementForm: !this.state.showElementForm,
+      selectedElement: null,
+    })
+  }
+
+  selectElement = (selectedElement: Element) => {
+    if (selectedElement.type !== 'CUSTOM') return
+    this.setState({
+      selectedElement,
+      showElementForm: true,
+    })
   }
 
   render () {
-    const { elements, onElementCreate } = this.props
+    const { elements, onSave } = this.props
+    const { showElementForm, selectedElement } = this.state
     return (
       <Content>
         <List>
           {elements.map(element =>
-            <ElementListItem key={element.id} element={element} />
+            <ElementListItem
+              key={element.id}
+              element={element}
+              onClick={() => this.selectElement(element)}
+            />
           )}
         </List>
         <ListItemButton onClick={this.toggleElementForm}>
           + element
         </ListItemButton>
-        {this.state.showElementForm && (
+        {showElementForm && (
           <EditElement
+            element={selectedElement}
             onCancel={this.toggleElementForm}
             onSave={element => {
               this.toggleElementForm()
-              onElementCreate(element)
+              onSave(element)
             }}
           />
         )}
