82c45ebebfdb64542b625eb3134c67e645fce361
Michel Mattos <mattos.michel@gmail.com>
2018-03-30 11:39:03 +1300

    Refactored some tests

--- src/locationCrafter/components/__tests__/ElementList.test.js
+++ src/locationCrafter/components/__tests__/ElementList.test.js
@@ -1,33 +1,67 @@
 // @flow
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 import ElementList from '../ElementList'
 
 const addElementSelector = '[data-test="add-element"]'
 
 test('should render a list of elements', () => {
   const props = createProps()
-
   const wrapper = shallow(<ElementList {...props} />)
-
-  expect(wrapper.find('ElementListItem').length).toEqual(props.elements.length)
-})
-
-test('should not render an editor', () => {
-  const props = createProps()
-
-  const wrapper = shallow(<ElementList {...props} />)
-
-  expect(wrapper.find('ElementEditor').length).toEqual(0)
+  expect(wrapper.find('ElementListItem').length).toBe(6)
 })
 
 test('should render an editor when add-element is clicked', () => {
   const props = createProps()
   const wrapper = shallow(<ElementList {...props} />)
-
+  expect(wrapper.find('ElementEditor')).not.toExist()
   wrapper.find(addElementSelector).simulate('click')
+  expect(wrapper.find('ElementEditor')).toExist()
+})
 
-  expect(wrapper.find('ElementEditor').length).toEqual(1)
+test('should render an editor when a custom element is selected', () => {
+  const customElement = { id: 0, type: 'CUSTOM', name: 'House', unique: true }
+  const props = createProps({ elements: [customElement] })
+  const wrapper = shallow(<ElementList {...props} />)
+  expect(wrapper.find('ElementEditor')).not.toExist()
+  wrapper.find('ElementListItem').simulate('click')
+  expect(wrapper.find('ElementEditor')).toExist()
+  expect(wrapper.find('ElementEditor')).toHaveProp('element', customElement)
+})
+
+test('should not render an editor when a non custom element is selected', () => {
+  const nonCustomElement = { id: 0, type: 'unknown', name: 'House', unique: true }
+  const props = createProps({ elements: [nonCustomElement] })
+  const wrapper = shallow(<ElementList {...props} />)
+  expect(wrapper.find('ElementEditor')).not.toExist()
+  wrapper.find('ElementListItem').simulate('click')
+  expect(wrapper.find('ElementEditor')).not.toExist()
+})
+
+describe('when an element (type != CUSTOM) is selected', () => {
+  let props, wrapper
+
+  beforeEach(() => {
+    props = createProps({
+      elements: [{ id: 1, type: 'NONE', name: 'None', unique: false }]
+    })
+    wrapper = shallow(<ElementList {...props} />)
+    wrapper.find('ElementListItem').props().onClick()
+    wrapper.update()
+  })
+
+  test('should not render an editor', () => {
+    expect(wrapper.find('ElementEditor').length).toEqual(0)
+  })
+})
+
+test('should call onSave when an element is saved', () => {
+  const props = createProps()
+  const wrapper = shallow(<ElementList {...props} />)
+  wrapper.find('ElementListItem').at(0).props().onClick()
+  wrapper.update()
+  wrapper.find('ElementEditor').props().onSave(props.elements[0])
+  expect(props.onSave).toHaveBeenCalledWith(props.elements[0])
 })
 
 function createProps (overrides = {}) {
