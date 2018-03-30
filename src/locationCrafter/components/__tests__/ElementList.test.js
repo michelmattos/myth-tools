1e4c86e9b455dd3ba7cc41c1796613e2f1cc054b
Michel Mattos <mattos.michel@gmail.com>
2018-03-30 13:48:58 +1300

    Refactored the tests to use selectors

--- src/locationCrafter/components/__tests__/ElementList.test.js
+++ src/locationCrafter/components/__tests__/ElementList.test.js
@@ -1,67 +1,56 @@
 // @flow
 import React from 'react'
 import { shallow, mount } from 'enzyme'
-import ElementList from '../ElementList'
+import ElementList, { testUtils as elementListTestUtils } from '../ElementList'
+import { testUtils as elementDetailsFormTestUtils } from '../ElementDetailsForm'
 
-const addElementSelector = '[data-test="add-element"]'
+const {
+  elementSelector,
+  addElementSelector,
+  editorSelector
+} = elementListTestUtils
+const { formSelector } = elementDetailsFormTestUtils
 
 test('should render a list of elements', () => {
   const props = createProps()
   const wrapper = shallow(<ElementList {...props} />)
-  expect(wrapper.find('ElementListItem').length).toBe(6)
+  expect(wrapper.find(elementSelector).length).toBe(6)
 })
 
 test('should render an editor when add-element is clicked', () => {
   const props = createProps()
   const wrapper = shallow(<ElementList {...props} />)
-  expect(wrapper.find('ElementEditor')).not.toExist()
+  expect(wrapper.find(editorSelector)).not.toExist()
   wrapper.find(addElementSelector).simulate('click')
-  expect(wrapper.find('ElementEditor')).toExist()
+  expect(wrapper.find(editorSelector)).toExist()
 })
 
 test('should render an editor when a custom element is selected', () => {
   const customElement = { id: 0, type: 'CUSTOM', name: 'House', unique: true }
   const props = createProps({ elements: [customElement] })
   const wrapper = shallow(<ElementList {...props} />)
-  expect(wrapper.find('ElementEditor')).not.toExist()
-  wrapper.find('ElementListItem').simulate('click')
-  expect(wrapper.find('ElementEditor')).toExist()
-  expect(wrapper.find('ElementEditor')).toHaveProp('element', customElement)
+  expect(wrapper.find(editorSelector)).not.toExist()
+  wrapper.find(elementSelector).simulate('click')
+  expect(wrapper.find(editorSelector)).toExist()
+  expect(wrapper.find(editorSelector)).toHaveProp('element', customElement)
 })
 
 test('should not render an editor when a non custom element is selected', () => {
   const nonCustomElement = { id: 0, type: 'NONE', name: 'None', unique: false }
   const props = createProps({ elements: [nonCustomElement] })
   const wrapper = shallow(<ElementList {...props} />)
-  expect(wrapper.find('ElementEditor')).not.toExist()
-  wrapper.find('ElementListItem').simulate('click')
-  expect(wrapper.find('ElementEditor')).not.toExist()
+  expect(wrapper.find(editorSelector)).not.toExist()
+  wrapper.find(elementSelector).simulate('click')
+  expect(wrapper.find(editorSelector)).not.toExist()
 })
 
-describe('when an element (type != CUSTOM) is selected', () => {
-  let props, wrapper
-
-  beforeEach(() => {
-    props = createProps({
-      elements: [{ id: 1, type: 'NONE', name: 'None', unique: false }]
-    })
-    wrapper = shallow(<ElementList {...props} />)
-    wrapper.find('ElementListItem').props().onClick()
-    wrapper.update()
-  })
-
-  test('should not render an editor', () => {
-    expect(wrapper.find('ElementEditor').length).toEqual(0)
-  })
-})
-
-test('should call onSave when an element is saved', () => {
-  const props = createProps()
-  const wrapper = shallow(<ElementList {...props} />)
-  wrapper.find('ElementListItem').at(0).props().onClick()
-  wrapper.update()
-  wrapper.find('ElementEditor').props().onSave(props.elements[0])
-  expect(props.onSave).toHaveBeenCalledWith(props.elements[0])
+test('should call props.onSave when an element is saved', () => {
+  const element = { id: 0, type: 'CUSTOM', name: 'House', unique: true }
+  const props = createProps({ elements: [element] })
+  const wrapper = mount(<ElementList {...props} />)
+  wrapper.find(elementSelector).simulate('click')
+  wrapper.find(formSelector).simulate('submit')
+  expect(props.onSave).toHaveBeenCalledWith(element)
 })
 
 function createProps (overrides = {}) {
